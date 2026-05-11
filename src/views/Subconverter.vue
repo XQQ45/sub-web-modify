c0a62cdd204ab0de2ee29fed7d71f60a1715af03
XQQ45 <122511465+XQQ45@users.noreply.github.com>
2026-05-11 16:57:14 +0800

    Add files via upload

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -338,7 +338,7 @@ export default {
           "suo.yt": "https://suo.yt/short",
         },
         customBackend: {
-          "萌新后端": "https://change.mxsubapi.dpdns.org",
+          "肥羊后端": "https://api.v1.mk",
 		  "CM负载均衡后端【vless reality+hy1+hy2】": "https://subapi.cmliussss.net",
           "CM应急备用后端【vless reality+hy1+hy2】": "https://subapi.fxxk.dedyn.io",
           "肥羊增强型后端【vless reality+hy1+hy2】": "https://url.v1.mk",
@@ -785,7 +785,7 @@ export default {
       form: {
         sourceSubUrl: "",
         clientType: "",
-        customBackend: this.getUrlParam() == "" ? "https://change.mxsubapi.dpdns.org" : this.getUrlParam(),
+        customBackend: this.getUrlParam() == "" ? "https://api.v1.mk" : this.getUrlParam(),
         shortType: "https://v1.mk/short",
         remoteConfig: "https://raw.githubusercontent.com/XQQ45/proxies/rule/config.ini",
         excludeRemarks: "",
